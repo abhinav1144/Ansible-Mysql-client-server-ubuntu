12bb2e95dad14f004f750383508c0b961d4f09ac
Abhinav Vemula <abvemula@osius.com>
2021-09-02 09:39:19 +0530

    permissions added

--- main/db.uninstall.sh
+++ main/db.uninstall.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo systemctl stop mysql
 sudo apt-get purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-* -y
 sudo rm -rf /etc/mysql /var/lib/mysql
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 cp -rf client-mysql.yml server-mysql.yml /etc/ansible/
 cp -rf mysqld.cnf /root
